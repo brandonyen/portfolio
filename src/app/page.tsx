08e30cbd84a2141e032263b82f591f780ea01bed
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2023-08-15 18:24:51 -0700

    created home page and initial text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
 "use client";
+import "./style.css";
+import { Heading } from "@chakra-ui/react";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="mainContainerTop">
+        <Heading>test</Heading>
+      </div>
+      <div className="mainContainer">
+        <Heading>test</Heading>
+      </div>
+      <div className="mainContainer">
+        <Heading>test</Heading>
+      </div>
+      <div>
+        <div className="mainChild1">
+          <Heading>I am</Heading>
+        </div>
+        <div className="mainChild2">
+          <Heading>Brandon Yen</Heading>
+        </div>
+      </div>
+      <div className="mainContainer">
+        <Heading>test</Heading>
+      </div>
+      <div className="mainContainer">
+        <Heading>test</Heading>
+      </div>
+      <div className="mainContainer">
+        <Heading>test</Heading>
+      </div>
+    </main>
+  );
 }
