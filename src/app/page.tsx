8058a5a8ba868c68f99d72126a155bbd875322e4
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2023-09-01 17:14:05 -0400

    changed layout and added a section for icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,15 @@
 "use client";
 import "./style.css";
-import { Heading } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <main>
-      <div className="mainContainer">
-        <div className="mainChild">
-          <text>A Software Developer</text>
+      <div className="divContainer">
+        <div className="iconContainer">
+          <div className="icons"></div>
         </div>
-        <div className="mainChild">
-          <text>A Content Creator</text>
-        </div>
-        <div className="mainChild">
-          <text>An Entrepreneur</text>
-        </div>
-        <div>
-          <div className="mainChild1">
-            <text>I am</text>
-          </div>
-          <div className="mainChild2">
-            <text>Brandon Yen</text>
-          </div>
-        </div>
-        <div className="mainChild">
-          <text>A Photographer</text>
-        </div>
-        <div className="mainChild">
-          <text>A Graphic Designer</text>
-        </div>
-        <div className="mainChild">
-          <text>A Humanitarian</text>
-        </div>
+        <div className="secondaryTextContainer"></div>
+        <div className="primaryTextContainer"></div>
       </div>
     </main>
   );
