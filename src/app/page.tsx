3ba8c68792973a2f3361ea3ed1c0aa38fadac92a
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2023-09-02 17:21:47 -0400

    added all stylized text and icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,39 @@ export default function Home() {
     <main>
       <div className="divContainer">
         <div className="iconContainer">
-          <div className="icons"></div>
+          <div className="icons">
+            <img
+              src="/images/email.png"
+              height="10%"
+              width="10%"
+              className="iconImage"
+            ></img>
+            <img
+              src="/images/linkedin.png"
+              height="10%"
+              width="10%"
+              className="iconImage"
+            ></img>
+            <img
+              src="/images/resume.png"
+              height="10%"
+              width="10%"
+              className="iconImage"
+            ></img>
+          </div>
         </div>
-        <div className="secondaryTextContainer"></div>
-        <div className="primaryTextContainer"></div>
+        <div className="secondaryTextContainer">
+          <text className="mainText">I am</text>
+        </div>
+        <div className="primaryTextContainer">
+          <text className="offText">A Software Developer</text>
+          <text className="offText">A Content Creator</text>
+          <text className="offText">An Entrepreneur</text>
+          <text className="mainText">Brandon Yen</text>
+          <text className="offText">A Photographer</text>
+          <text className="offText">A Graphic Designer</text>
+          <text className="offText">A Humanitarian</text>
+        </div>
       </div>
     </main>
   );
