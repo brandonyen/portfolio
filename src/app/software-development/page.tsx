1d02f534c4fc6fbc569b00d68fc9a1ec884c93c9
Brandon Yen <28814770+brandonyen@users.noreply.github.com>
2023-09-18 17:40:51 -0400

    last commit before changing to new layout

--- src/app/software-development/page.tsx
+++ src/app/software-development/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 import "./style.css";
 import Link from "next/link";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Code } from "@chakra-ui/react";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="navBarContainer"></div>
+      <div className="mainContainer"></div>
+    </main>
+  );
 }
